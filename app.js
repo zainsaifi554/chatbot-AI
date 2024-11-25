82f0b8e51037d452c3fdb525d257b44c12f979e1
zainsaifi554 <zainsaifi235798@gmail.com>
2024-11-25 22:45:35 +0500

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-alert("HELLO WELCOME TO 'YOUR'S APP'")
+// alert("HELLO WELCOME TO 'YOUR'S APP'")
 
 
 var list = document.getElementById("list");
@@ -87,48 +87,123 @@ function sendMessage() {
         }, 2000);
         userInput.value = "";
 
-    } else if (["mujhy entertain karo please", "mjy entertain karo plz", "mujhy entertain karo","mujhy entertainment do","entertain karo", "netertain kr k dikaio mujhy ", "entertain krna hai mujhy", "boryt k liye kch karo"].indexOf(value.toLowerCase()) !== -1) {
+    } else if (["mujhy entertain karo please","videos","send me videos", "mjy entertain karo plz", "mujhy entertain karo","mujhy entertainment do","entertain karo", "netertain kr k dikaio mujhy ", "entertain krna hai mujhy", "boryt k liye kch karo"].indexOf(value.toLowerCase()) !== -1) {
+            // setTimeout(function () {
+            //     list.innerHTML += "<li class='left-side'> <img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br><img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+            // }, 2000);
+            // setTimeout(function () {
+            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+            //     typing.style.display = "none";
+            // }, 4000);
+            // setTimeout(function () {
+            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+            //     typing.style.display = "none";
+            // }, 4000);
+            // setTimeout(function () {
+            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+            //     typing.style.display = "none";
+            // }, 4000);
             setTimeout(function () {
-                list.innerHTML += "<li class='left-side'> <img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br><img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            }, 2000);
-            setTimeout(function () {
-                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <video src='images/5.mp4' width='350' height='250' controls></video><br></li>";
                 typing.style.display = "none";
             }, 4000);
             setTimeout(function () {
-                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <video src='images/4.mp4' width='350' height='250' controls></video><br></li>";
                 typing.style.display = "none";
             }, 4000);
-            // setTimeout(function () {
-            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            //     typing.style.display = "none";
-            // }, 4000);
-            // setTimeout(function () {
-            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            //     typing.style.display = "none";
-            // }, 4000);
-            // setTimeout(function () {
-            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            //     typing.style.display = "none";
-            // }, 4000);
-            // setTimeout(function () {
-            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            //     typing.style.display = "none";
-            // }, 4000);
-            // setTimeout(function () {
-            //     list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/cyborg.png' alt='' width='300px' height='250px'><br></li>";
-            //     typing.style.display = "none";
-            // }, 4000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <video src='images/3.mp4' width='350' height='250' controls></video><br></li>";
+                typing.style.display = "none";
+            }, 4000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <video src='images/2.mp4' width='350' height='250' controls></video><br></li>";
+                typing.style.display = "none";
+            }, 4000);
             setTimeout(function () {
                 list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> <video src='images/1.mp4' width='350' height='250' controls></video><br></li>";
                 typing.style.display = "none";
             }, 4000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br>please see all videos<br></li>";
+                typing.style.display = "none";
+            }, 4000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br>maza nh aya tw please mujhy nichy type karen poetry k liye same text wording on below<br></li>";
+                typing.style.display = "none";
+            }, 4000);
+                 setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br>please send me poetry<br></li>";
+                typing.style.display = "none";
+            }, 4000);
+          
+
             userInput.value = "";
             
+        } else if (["please send me poetry","poetry", "plz send me poetry", "send poetry", "poetry sunao", "you send me poetry"].indexOf(value.toLowerCase()) !== -1) {
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/6.jpeg' alt='' width='300px' height='150px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/7.jpeg' alt='' width='300px' height='350px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/8.jpeg' alt='' width='300px' height='350px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/9.jpeg' alt='' width='300px' height='350px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/10.jpeg' alt='' width='300px' height='350px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> <img src='images/11.jpeg' alt='' width='300px' height='350px'></li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> you will please see all images </li>";
+                typing.style.display = "none";
+            }, 2000);
+            userInput.value = "";
+
+        } else if (["allah hafiz","allahafiz", "allahhafiz", "allah  hafiz", "  allah hafiz", "khuda hafiz"].indexOf(value.toLowerCase()) !== -1) {
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> sorry to say mn apko abhi zyda kch entertain nh ke saka</li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> sorry or meri koi bt buri lagi ho tw mene kch glt keh diya ho to really really sorry</li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> naraz nh hona please bs abhi kch thk nh ab itna hi kr skta hon mn</li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> thanks for your pleasure ap sy bt kr k acha laga</li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br> acha chalta hon khuwabon kyayalon sarkon ya raston mn milta hon</li>";
+                typing.style.display = "none";
+            }, 2000);
+            setTimeout(function () {
+                list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt=''style='border-radius: 50%;' width='30px' height='30px'><br>dua mn yad rakhna allah hafiz bs ab agy ab ap kch type nh krna wrna mn naraz ho jaonga </li>";
+                typing.style.display = "none";
+            }, 2000);
+
 
     } else {
         setTimeout(function () {
-            list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> Mazrat mein aapki baat smjh nhi saka</li>";
+            list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> sorry main abhi nichy diye gaye text k jawab dy sakta hon</li>";
+            typing.style.display = "none";
+        }, 2000);
+        setTimeout(function () {
+            list.innerHTML += "<li class='left-side'><img src='images/cyborg.png' alt='' style='border-radius: 50%;' width='30px' height='30px'><br> abhi in sbh k jawab de skta hon mn so please ask me about same like below <br> assalamualikum <br> mujhy entertain karo <br> allah hafiz </li>";
             typing.style.display = "none";
         }, 2000);
         userInput.value = "";
